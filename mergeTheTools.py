83b676825600aca9f8c13b80ae2f1f76ab40e355
viikkkas <61241966+viikkkas@users.noreply.github.com>
2020-06-15 17:58:20 +0530

    Create mergeTheTools.py

---
+++ mergeTheTools.py
@@ -0,0 +1,21 @@
+def remove_dup(word):
+    new_word = ''
+    for i in word:
+        if i in new_word:
+            continue
+        else:
+            new_word +=i
+    return new_word
+
+
+def merge_the_tools(string, k):
+    length = len(string)
+    divi = int(k)
+    list1 = []
+    for i in range(0, length, divi):
+        temp = string[i:i + divi]
+        list1.append(temp)
+
+    for word in list1:
+        temp = remove_dup(word)
+        print(temp)
